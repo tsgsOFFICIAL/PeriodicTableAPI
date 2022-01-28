ba072385f23b22d464311e0b4cd9ef29eb5a282d
Marcus Jensen <marcusproman0507@gmail.com>
2022-01-28 10:48:15 +0100

    Add project files.

---
+++ PeriodicTableAPI/Controllers/ElementsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PeriodicTableAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ElementsController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public string Get()
+        {
+            return System.IO.File.ReadAllText("data/elements.json");
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public IEnumerable<string> Get(int id)
+        {
+            return new string[] { $"value{id}" };
+        }
+    }
+}
---
+++ PeriodicTableAPI/Program.cs
@@ -0,0 +1,36 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins"; 
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod();
+        });
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
